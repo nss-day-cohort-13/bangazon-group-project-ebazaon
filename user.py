11260d7820dbe4f07f6f6545b3e0596d7fd98412
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-08-08 13:37:16 -0500

    passed user testing and touched .gitignore and serialization.py

---
+++ user.py
@@ -0,0 +1,19 @@
+import uuid
+
+
+class User:
+  ''' Creates a user with the following attributes passed in below in the __init__ method
+      line 14 creates a unique id for each customer created '''
+  def __init__(self, name, address, city, state, zipcode, phone):
+    self.name = name
+    self.address = address
+    self.city = city
+    self.state = state
+    self.zipcode = zipcode
+    self.phone = phone
+    self.uuid = uuid.uuid4()
+
+
+
+
+
